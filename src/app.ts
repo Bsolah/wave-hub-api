a53b43a6823e5dd8d3ed8d2c984a59bd8653d175
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2024-11-20 22:48:15 +0100

    update on user service

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,35 @@
-import express, { Application } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import sequelize from './config/database';
-import userRoutes from './routes/userRoutes';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./config/database";
+import userRoutes from "./routes/userRoutes";
+import errorMiddleware from "./middleware/error.middleware";
+import errHandler from "./errHandler";
 
 dotenv.config();
 
-const app: Application = express();
+const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  errorMiddleware(err, req, res, next);
+});
 
 // Test DB Connection
-sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch((err) => console.log('Error: ' + err));
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log("Error: " + err));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,5 @@
 import UserRepository from "../repository/user.repository";
+import { BaseError } from "../utils/error";
 
 class UserService {
   private userRepository: UserRepository;
@@ -10,42 +11,54 @@ class UserService {
     try {
       const user = await this.userRepository.createUser(data);
       return user;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 
   async getAllUsers() {
     try {
       const users = await this.userRepository.getAllUsers();
       return users;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 
   async getUserById(userId: string) {
     try {
       const user = await this.userRepository.getUserById(userId);
       return user;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 
   async getUserByEmail(email: string) {
     try {
       const user = await this.userRepository.getUserByEmail(email);
       return user;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 
   async updateUser(userId: string, data: any) {
     try {
       const user = await this.userRepository.updateUser(userId, data);
       return user;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 
   async deactivateUser(userId: string) {
     try {
       const user = await this.userRepository.deactivateUser(userId);
       return user;
-    } catch (error) {}
+    } catch (error: any) {
+      throw new BaseError(error, 400);
+    }
   }
 }
 
