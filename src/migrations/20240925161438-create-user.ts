f240543988f5d0d4aafc896c1bf342ca73945fce
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2024-11-22 09:44:46 +0100

    update on code

---
+++ src/migrations/20240925161438-create-user.ts
@@ -0,0 +1,50 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.createTable("users", {
+      user_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.dropTable("users");
+  },
+};
